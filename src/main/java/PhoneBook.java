c4804572da6236f8cd2be21282f4acc7570f2ea7
Vladimir Komarov <vvkcrtc@gmail.com>
2022-12-15 23:29:14 +0300

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,6 @@
 public class PhoneBook {
+    public int add(String name, String phoneNumber) {
+        return 0;
+    }
+
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,17 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class PhoneBookTest {
+    @Test
+    public void testAdd() {
+        PhoneBook pb = new PhoneBook();
+        int maxCount = 10;
+        for(int i = 0; i < maxCount; i++) {
+            pb.add("Name"+i, "+7911111111"+i);
+        }
+        int count = pb.add("Name0","");
+
+        Assertions.assertTrue(count == maxCount, "Ошибка добавления");
+
+    }
 }
